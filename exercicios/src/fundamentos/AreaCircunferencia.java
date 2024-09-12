bd9b0539255a58082f78c4f00c59f2066e349fef
Renato Gomes <Renato Gomes@DESKTOP-9P1KUNQ>
2024-09-12 16:55:01 -0300

    Aulas 20,21,22,23

---
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+	
+	public static void main(String[] args) {
+		//21 Variaveis e Constantes
+		double raio = 3.4;
+		final double PI = 3.14159;
+		
+		double area = PI * raio * raio;
+		
+		System.out.printf("A area da circunferencia: %fm²", area);
+	}
+}
--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,21 @@
 package fundamentos;
-
+/**
+ * Essa classe representa...Javadoc
+ * 
+ * @author Renato Gomes
+ */
 public class PrimeiroPrograma {
-
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		
+		// Uma sentenção de codigo terminar com ;
+		/*
+		 * Linha 1
+		 * Linha 2
+		 */
 		System.out.println("Primeiro Programa Parte #01!!!");
 		System.out.println("Primeiro Programa Parte #02!!!");
 	}
