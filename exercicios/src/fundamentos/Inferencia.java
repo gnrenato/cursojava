29073a4f2cdb42a77f9e1fdbc860b9d0d09b1948
Renato Gomes <Renato Gomes@DESKTOP-9P1KUNQ>
2024-09-13 11:49:40 -0300

    24 - Inferencia de tipos

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+		//24 - Inferencia de tipos
+		double a = 4.2;
+		var b = 4.4;
+		var c = "Teste";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
