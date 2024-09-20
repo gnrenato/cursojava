1c0b63170d8c4437cdd9d79396c3022ee3ea3946
Renato Gomes <Renato Gomes@DESKTOP-9P1KUNQ>
2024-09-20 15:35:02 -0300

    32 - Objeto vs Primitivo

---
+++ exercicios/src/fundamentos/PrimitivoVsObjetos.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class PrimitivoVsObjetos {
+	
+	public static void main(String[] args) {
+		// 32 - Objeto vs Primitivo
+		// Tudo em java é objeto menos os 8 tipos primitivos
+		// Nos tipos primitivos não temos o os metodos (comportamentos associados) igual aos objetos
+		// os tipos primitivos tem uma funcionalidade que chama Wrappers
+		String s = "texto";
+		s.toUpperCase();
+		
+		int a = 2;
+		
+		
+	}
+}
