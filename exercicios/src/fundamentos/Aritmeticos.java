6d3dd95f6d695f2ec64e935da6e1736d4bceab1e
Renato Gomes <Renato Gomes@DESKTOP-NR3R820>
2024-10-01 19:44:36 -0300

    34 - Operadores Aritmeticos

---
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -0,0 +1,31 @@
+package fundamentos;
+
+public class Aritmeticos {
+	// 34 - Operadores Aritmeticos
+	public static void main(String[] args) {
+		
+		System.out.println(2 + 3); // Soma de valores literais
+		
+		var x = 34.56; // criando a variavel usando inferencia, de acordo com o valor é um double
+		double y = 2.2; // criando variavel com do tipo double
+		
+		System.out.println(x + y); // Soma de valores das variaveis
+		System.out.println(x - y); // Subtração de valores das variaveis
+		System.out.println(x * y); // Multiplicação de valores das variaveis
+		System.out.println(x / y); // Divisão de valores das variaveis
+		
+		// Operações utilizando int
+		int  a = 8;
+		int b = 3;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a / (double) b); // realizando uma conversão para double usando Cast
+		
+		System.out.println(8 % 3); // Usando o operado modulo para resultar no resto da divisão
+		System.out.println(a % b);
+		
+	}
+}
