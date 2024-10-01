0c164dd2c674c630f563c62b3cb20bea424e9252
Renato Gomes <Renato Gomes@DESKTOP-NR3R820>
2024-10-01 20:09:12 -0300

    // 44 - Desafio Operadores Aritmeticos

---
+++ exercicios/src/fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class DesafioAritmeticos {
+	// 44 - Desafio Operadores Aritmeticos
+	public static void main(String[] args) {
+		
+		double a1 = Math.pow(6 * (3 + 2), 2);
+		double b1 = a1 / (3*2);
+		
+		double a2 = Math.pow((( 1 - 5 ) * ( 2 - 7 )) / 2, 2);
+		
+		double resultado = Math.pow(b1 - a2, 3) / Math.pow(10, 3);
+		
+		System.out.println(resultado);
+	}
+}
