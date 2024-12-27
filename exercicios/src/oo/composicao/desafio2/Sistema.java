9ca15f51564fa3c2338ab0d889c499194d98c7ab
Renato Gomes Neto <gn.renato@hotmail.com>
2024-12-27 11:02:55 -0300

    exercicio OO

--- exercicios/src/oo/composicao/desafio2/Sistema.java
+++ exercicios/src/oo/composicao/desafio2/Sistema.java
@@ -3,6 +3,13 @@ package oo.composicao.desafio2;
 public class Sistema {
 
 	public static void main(String[] args) {
+
+		Compra compra1 = new Compra();
+		compra1.adicionarItem(new Produto("Caneta", 10), 10);
 		
+		Cliente cliente1 = new Cliente("Renato", compra1);
+		
+		
+
 	}
 }
