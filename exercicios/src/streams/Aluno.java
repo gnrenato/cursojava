60efccbffa7c066ddf42ab149ea7cbde32ae59ae
Renato Gomes Neto <gn.renato@hotmail.com>
2025-02-11 16:36:30 -0300

    // 209 Reduce 03

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -5,10 +5,16 @@ public class Aluno {
 
 	final String nome;
 	final double nota;
+	final boolean bomComportmento;
 
 	public Aluno(String nome, double nota) {
+		this(nome, nota, true);
+	}
+
+	public Aluno(String nome, double nota, boolean bomComportmento) {
 		this.nome = nome;
 		this.nota = nota;
+		this.bomComportmento = bomComportmento;
 	}
 
 }
