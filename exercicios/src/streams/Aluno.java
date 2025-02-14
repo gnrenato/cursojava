53817f3b20dfdcd4ae50265a3ff8715c5de2dbe0
Renato Gomes Neto <gn.renato@hotmail.com>
2025-02-14 14:41:17 -0300

    // 214 Outros Metodos

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -21,4 +21,38 @@ public class Aluno {
 		return nome + " Tem a nota " + nota;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (bomComportmento ? 1231 : 1237);
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (bomComportmento != other.bomComportmento)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		return true;
+	}
+
+	
 }
