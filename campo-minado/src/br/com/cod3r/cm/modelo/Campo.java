e9af1b2eb8cfc87d5f1f061dd9417f9618513274
Renato Gomes Neto <gn.renato@hotmail.com>
2025-04-24 10:36:27 -0300

    porjeto campo minado

---
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -0,0 +1,42 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+	private final int linha;
+	private final int coluna;
+
+	// posso colocar excplicito o valor inicial, mas por padrão o boolean é false
+	private boolean aberto;
+	private boolean minado;
+	private boolean marcado;
+
+	private List<Campo> vizinhos = new ArrayList<>();
+
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
