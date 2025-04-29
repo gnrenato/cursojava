5bfd333af64febc231cd8f0147e040818d82c592
Renato Gomes <Renato Gomes@DESKTOP-ERRD4SC>
2025-04-29 19:47:18 -0300

    241 Classe Tabuleiro 3

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -10,19 +10,26 @@ public class Tabuleiro {
 	private int linhas;
 	private int colunas;
 	private int minas;
-	
+
 	private final List<Campo> campos = new ArrayList<>();
 
 	public Tabuleiro(int linhas, int colunas, int minas) {
 		this.linhas = linhas;
 		this.colunas = colunas;
 		this.minas = minas;
-		
+
 		gerarCampos();
 		associarVizinhos();
 		sortearMinas();
 	}
-
+	
+	public void abrir(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst().ifPresent(c -> c.abrir());
+	}
+	
+	public void alternarMarcação(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst().ifPresent(c -> c.alternarMarcação());
+	}
 
 	private void gerarCampos() {
 		for (int l = 0; l < linhas; l++) {
@@ -31,7 +38,7 @@ public class Tabuleiro {
 			}
 		}
 	}
-	
+
 	private void associarVizinhos() {
 		for (Campo c1 : campos) {
 			for (Campo c2 : campos) {
@@ -39,25 +46,44 @@ public class Tabuleiro {
 			}
 		}
 	}
-	
+
 	private void sortearMinas() {
 		long minasArmadas = 0;
 		Predicate<Campo> minado = c -> c.isMinado();
-		
+
 		do {
 			minasArmadas = campos.stream().filter(minado).count();
-			int aleatorio = (int) (Math.random() * campos.size()); //prioridade de cast utilizando () como na matematica
+			int aleatorio = (int) (Math.random() * campos.size()); // prioridade de cast utilizando () como na
+																	// matematica
 			campos.get(aleatorio).minar();
 		} while (minasArmadas < minas);
 	}
-	
+
 	public boolean objetivoAlcancado() {
 		return campos.stream().allMatch(c -> c.objetivoAlcancado());
 	}
-	
+
 	public void reiniciar() {
 		campos.stream().forEach(c -> c.reiniciar());
 		sortearMinas();
 	}
-	
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		int i = 0;
+		for (int l = 0; l < linhas; l++) {
+
+			for (int c = 0; c < colunas; c++) {
+				sb.append(" ");
+				sb.append(campos.get(i));				
+				sb.append(" ");
+				i++;
+			}
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+
 }
